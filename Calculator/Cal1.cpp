2a33cd9674046b12de9d426f131917942c8c8792
조송하 <102511282+Song-haJo@users.noreply.github.com>
2022-09-07 21:08:47 +0900

    Update Cal1.cpp

--- Calculator/Cal1.cpp
+++ Calculator/Cal1.cpp
@@ -2,8 +2,12 @@ void CCalculator1Dlg::OnBnClickedButton1()
 {
 	// TODO: Add your control notification handler code here
 	int a, b, c;
-	a = GetDlgItemInt(IDC_A);
-	b = GetDlgItemInt(IDC_B);
+	a = GetDlgItemInt(IDC_EDIT1);	// Get an integer from IDC_EDIT1.
+	b = GetDlgItemInt(IDC_EDIT2);	// Get an integer from IDC_EDIT2.
 	c = a + b;
-	SetDlgItemInt(IDC_C, c);
+	SetDlgItemInt(IDC_EDIT3, c);	// Put the result in IDC_EDIT3.
 }
+
+// IDC_EDIT1 (Edit Control): The edit control added for the first.
+// IDC_EDIT2 (Edit Control): The edit control added for the second.
+// IDC_EDIT3 (Edit Control): The edit control added for the second.
