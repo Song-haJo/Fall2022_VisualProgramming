01120cae0832315c0239c053a80e530f4e9aac89
조송하 <102511282+Song-haJo@users.noreply.github.com>
2022-09-08 14:11:36 +0900

    Create Custom Pen.cpp

---
+++ Custom Pen.cpp
@@ -0,0 +1,42 @@
+int n;
+void CPenwThicknessSlidebarDlg::OnNMCustomdrawSlider1(NMHDR* pNMHDR, LRESULT* pResult)
+{
+	LPNMCUSTOMDRAW pNMCD = reinterpret_cast<LPNMCUSTOMDRAW>(pNMHDR);
+	// TODO: Add your control notification handler code here
+	*pResult = 0;
+	CString pos;
+	n = m_Slider.GetPos();
+	pos.Format(L"%d", n);
+	SetWindowText(pos);
+}
+
+CPoint oldPt;									//pen
+COLORREF col;									//pen
+void CPenwThicknessSlidebarDlg::OnMouseMove(UINT nFlags, CPoint point)
+{
+	// TODO: Add your message handler code here and/or call default
+	if (nFlags == MK_LBUTTON)
+	{
+		CPen pen(PS_SOLID, n, col);				//pen
+		//CBrush br = RGB(0, 0, 255);				//ellipse
+		CClientDC dc(this);
+		//dc.SelectObject(&br);
+		dc.SelectObject(&pen);					//pen
+		dc.MoveTo(oldPt);						//pen
+		dc.LineTo(point);						//pen
+		//CPoint p = point;							//ellipse
+		//dc.Ellipse(p.x-n, p.y-n, p.x+n, p.y+n);	//ellipse
+		CDialogEx::OnMouseMove(nFlags, point);
+	}
+	oldPt = point;
+}
+
+void CPenwThicknessSlidebarDlg::OnBnClickedButton1()
+{
+	// TODO: Add your control notification handler code here
+	CColorDialog dlg;
+	if (dlg.DoModal() == IDOK)
+	{
+		col = dlg.GetColor();
+	}
+}
