ac03d839ad764e3301be0ed20e55661f088b402d
조송하 <102511282+Song-haJo@users.noreply.github.com>
2022-09-11 00:48:23 +0900

    Create Customizable Pen.cpp

---
+++ 2. Pen/Customizable Pen.cpp
@@ -0,0 +1,39 @@
+///////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////// First  Part /////////////////////////////////////
+
+int n;
+void CEarthwormDlg::OnNMCustomdrawSlider1(NMHDR* pNMHDR, LRESULT* pResult)
+{
+	LPNMCUSTOMDRAW pNMCD = reinterpret_cast<LPNMCUSTOMDRAW>(pNMHDR);
+	// TODO: Add your control notification handler code here
+	*pResult = 0;
+	CString pos;
+	n = sz_Slider.GetPos();
+	pos.Format(L"%d", n);
+	SetWindowText(pos);			// Show the value on the window title
+}
+
+
+///////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////// Second Part /////////////////////////////////////
+
+CPoint oldPt;
+COLORREF col;
+void CCustomizablePenDlg::OnMouseMove(UINT nFlags, CPoint point)
+{
+	// TODO: Add your message handler code here and/or call default
+	if (nFlags == MK_LBUTTON)
+	{
+		CPen pen(PS_SOLID, n, col);
+		CClientDC dc(this);
+		dc.SelectObject(&pen);
+		dc.MoveTo(oldPt);
+		dc.LineTo(point);
+		CDialogEx::OnMouseMove(nFlags, point);
+	}
+	oldPt = point;
+}
+
+
+///////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////// Third  Part /////////////////////////////////////
