d45a4efceb6002c4bc518945be481d6ccecc820f
조송하 <102511282+Song-haJo@users.noreply.github.com>
2022-09-11 00:35:02 +0900

    Update Earthworm.cpp

--- 2. Pen/Earthworm.cpp
+++ 2. Pen/Earthworm.cpp
@@ -10,9 +10,22 @@ void CEarthwormDlg::OnNMCustomdrawSlider1(NMHDR* pNMHDR, LRESULT* pResult)
 	CString pos;
 	n = sz_Slider.GetPos();
 	pos.Format(L"%d", n);
-	SetWindowText(pos);
+	SetWindowText(pos);			// Show the value on the window title
 }
 
 ///////////////////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////// Second Part /////////////////////////////////////
 
+void CEarthwormDlg::OnMouseMove(UINT nFlags, CPoint point)
+{
+	// TODO: Add your message handler code here and/or call default
+	if (nFlags == MK_LBUTTON)
+	{
+		CBrush br = RGB(0, 0, 255);	// set the ellipse color as blue
+		CClientDC dc(this);
+		dc.SelectObject(&br);
+		CPoint p = point;
+		dc.Ellipse(p.x - n, p.y - n, p.x + n, p.y + n);
+		CDialogEx::OnMouseMove(nFlags, point);
+	}
+}
