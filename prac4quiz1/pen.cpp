ef16e99e5282ba47716fe328b56c018b65314cad
조송하 <102511282+Song-haJo@users.noreply.github.com>
2022-09-15 17:45:43 +0900

    Update pen.cpp

--- prac4quiz1/pen.cpp
+++ prac4quiz1/pen.cpp
@@ -47,11 +47,11 @@ void Cmfc01Dlg::OnSel16()					// When you click the submenu 16, you can set your
 void Cmfc01Dlg::OnSelColor()					// When you click the submenu Color, you can use the Color Dialog.
 {
 	// TODO: Add your command handler code here
-	// 1안과 2안 중 하나만 적으면 된다.
-	// 1안
+	// Choose one between Option 1 and 2
+	// Option 1: works same as Option 2
 	OnBnClickedButton1();
 
-	// 2안
+	// Option 2: works same as Option 1
 	/*
 	CColorDialog dlg;
 	if (dlg.DoModal() == IDOK) {		// IDOK = OK Button in the Color Dialog 
@@ -61,12 +61,12 @@ void Cmfc01Dlg::OnSelColor()					// When you click the submenu Color, you can us
 }
 
 
-BOOL Cmfc01Dlg::PreTranslateMessage(MSG* pMsg)
+BOOL Cmfc01Dlg::PreTranslateMessage(MSG* pMsg)			// When you press the "R," "G," or "B" key, you can change the pen color to Red, Green, or Blue.
 {
 	// TODO: Add your specialized code here and/or call the base class
 	if (pMsg->message == WM_KEYDOWN) {
-		if (pMsg->wParam == 'R') m_Col = RGB(255, 0, 0);	// 대문자로 적어야 대소문자 모두 인식
-		if (pMsg->wParam == 'G') m_Col = RGB(0, 255, 0);
+		if (pMsg->wParam == 'R') m_Col = RGB(255, 0, 0);	// For the condition, you need to use capital letters
+		if (pMsg->wParam == 'G') m_Col = RGB(0, 255, 0);	// to let your computer recognize both upper and lower cases.
 		if (pMsg->wParam == 'B') m_Col = RGB(0, 0, 255);
 	}
 	return false;
@@ -74,7 +74,7 @@ BOOL Cmfc01Dlg::PreTranslateMessage(MSG* pMsg)
 }
 
 
-void Cmfc01Dlg::OnNMCustomdrawSlider1(NMHDR* pNMHDR, LRESULT* pResult)
+void Cmfc01Dlg::OnNMCustomdrawSlider1(NMHDR* pNMHDR, LRESULT* pResult)	// This will get you the number of the slider's position.
 {
 	LPNMCUSTOMDRAW pNMCD = reinterpret_cast<LPNMCUSTOMDRAW>(pNMHDR);
 	// TODO: Add your control notification handler code here
