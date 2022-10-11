2fcbab37bcc088c8675d8a1f15b9b92ecd2e0bff
조송하 <102511282+Song-haJo@users.noreply.github.com>
2022-10-11 20:55:44 +0900

    Create Calc.w_Lib.cpp

---
+++ 5. Calc. with Library/Calc.w_Lib.cpp
@@ -0,0 +1,15 @@
+/*Select [MFC Dynamic-Link Library] and click [Next].
+Keep the project name as "MFCLibrary1" and click [Create]. Then, set as below.
+	- [DLL type]: Regular DLL using shared MFC DLL
+Click [OK] and write as below in the {MFCLibrary1.def} file. (Solution Explorer>SourceFiles)
+-------------------------------------------------------------------------------------------------*/
+; MFCLibrary1.def : Declares the module parameters for the DLL.	//
+							//
+LIBRARY							//
+							//
+EXPORTS							//
+    ; Explicit exports can go here				//
+    Add							// C
+/*-------------------------------------------------------------------------------------------------
+
+
