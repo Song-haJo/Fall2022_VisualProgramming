23bffc5f95a51a343834355acbdca62c232c6ab1
조송하 <102511282+Song-haJo@users.noreply.github.com>
2022-10-11 20:56:31 +0900

    Update Calc.w_Lib.cpp

--- 5. Calc. with Library/Calc.w_Lib.cpp
+++ 5. Calc. with Library/Calc.w_Lib.cpp
@@ -4,12 +4,12 @@ Keep the project name as "MFCLibrary1" and click [Create]. Then, set as below.
 Click [OK] and write as below in the {MFCLibrary1.def} file. (Solution Explorer>SourceFiles)
 -------------------------------------------------------------------------------------------------*/
 ; MFCLibrary1.def : Declares the module parameters for the DLL.	//
-							//
-LIBRARY							//
-							//
-EXPORTS							//
+								//
+LIBRARY								//
+								//
+EXPORTS								//
     ; Explicit exports can go here				//
-    Add							// C
+    Add								// C
 /*-------------------------------------------------------------------------------------------------
 
 
